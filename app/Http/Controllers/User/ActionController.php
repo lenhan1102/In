6cbdd8ee9c5d66f0029c369196d8e85ec752d919
Le Nhan <lenhan1102igdu@gmail.com>
2017-05-17 00:09:36 +0700

    no planetB

--- app/Http/Controllers/User/ActionController.php
+++ app/Http/Controllers/User/ActionController.php
@@ -21,9 +21,6 @@ class ActionController extends Controller
 {
     public function vote(Request $request)
     {
-        if (Gate::denies('vote', Dish::find($request->dish_id))) {
-            return response('Insufficient permissions', 401);
-        }
         // update vote table
         preg_match('/star_([1-5]{1})/', $request->voted, $match);
         $voted = intval(substr($match[0], 5, 1));
